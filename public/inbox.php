82400042473b468b3c4c640be5f7be6380bb1568
Jemerito <101377289+Jemerito@users.noreply.github.com>
2024-01-12 16:12:28 +0800

    Update inbox.php

--- public/inbox.php
+++ public/inbox.php
@@ -1,71 +1,61 @@
 <?php
-$servername = "localhost";
-$username = "root";
-$password = "";
-$dbname = "face_recog";
+$db = mysqli_connect('localhost', 'root', '', 'face_recog');
 
-// Create a connection
-$conn = new mysqli($servername, $username, $password, $dbname);
-
-// Check the connection
-if ($conn->connect_error) {
-    die("Connection failed: " . $conn->connect_error);
+if (!$db) {
+    die("Connection failed: " . mysqli_connect_error());
 }
-
-// Fetch messages from the database
-$sql = "SELECT * FROM messages ORDER BY id DESC";
-$result = $conn->query($sql);
 ?>
 
 <!DOCTYPE html>
-<html lang="en">
-
+<html>
 <head>
-    <meta charset="UTF-8" />
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-    <link rel="stylesheet" href="css/inbox.css">
-    <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.15.4/css/all.css"
-        integrity="sha384-DyZ88mC6Up2uqS4h/KRgHuoeGwBcD4Ng9SiP4dIRy0EXTlnuz47vAwmeGwVChigm"
-        crossorigin="anonymous" />
-    <title>Inbox - Mad Plate's Sizzling House</title>
+    <title>Feedback</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
 </head>
+<style>
+    h1 {
+        font-family: "Times New Roman", Times, serif;
+        font-size: 80px;
+        text-align: center;
+        color: white;
+    }
+    table {
+        border-collapse: collapse;
+        margin:     left;
+        width: 500%;
+        border: 1px solid black;
+        text-align: center;
+    }
 
-<body>
-    <div class="container">
-        <!-- Include the sidebar and header from db.php -->
-        <?php include 'db.php'; ?>
+    th, td {
+        border: 1px solid black;
+        padding: 8px;
+    } 
+    body {
+        background-repeat: no-repeat;
+        background-size: 100%;
+    }
+</style>
+<body background="https://i.gifer.com/7tD0.gif">
+    <i><h1>Feedback</h1></i>
 
-        <!-- Main content for Inbox page -->
-        <div class="main">
-            <div class="cards">
-                <div class="card">
-                    <div class="messages-container">
-                        <h2>Inbox</h2>
-                        <?php
-                        if ($result->num_rows > 0) {
-                            // Output data of each row
-                            while ($row = $result->fetch_assoc()) {
-                                ?>
-                                <div class="message">
-                                    <p>Email: <?php echo $row['email']; ?></p>
-                                    <p>Opinion: <?php echo $row['opinion']; ?></p>
-                                    <p>Sent at: <?php echo $row['timestamp']; ?></p>
-                                </div>
-                                <?php
-                            }
-                        } else {
-                            echo "<p>No messages in the inbox.</p>";
-                        }
-                        ?>
-                    </div>
-                </div>
-            </div>
-        </div>
+    <table class="table table table-hover">
+        
+        <tr>
+            <th> G-mail</th>
+            <th> Feedback</th>
+        </tr>
 
-        <!-- Your other HTML content goes here -->
-    </div>
-    <script src="js/inbox.js"></script>
+        <?php 
+        $query = mysqli_query($db, "SELECT * FROM messages");               
+        while($value = mysqli_fetch_array($query)){
+            echo "<tr>"; 
+            echo "<td>". $value['email']. "</td>";
+            echo "<td>". $value['opinion']. "</td>";
+        }
+        ?>
+    </table>
+
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL" crossorigin="anonymous"></script>
 </body>
-
 </html>
